8ce948a88ebe3ff92b0deca50af32b947bc2e34b
Guilherme <61518973+jepesg@users.noreply.github.com>
2024-10-09 10:02:40 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+let offset = 0;
+let limit = 0;
+let maxRecords = 1000; 
+const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonList = document.getElementById('pokemonList')
+
+function convertListToHtml(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol id ="pokemonList" class="types">
+                    ${pokemon.types.map((type) => `<li class="type${type}">${type}</li>`).join('')}
+                </ol>
+                <img src=${pokemon.photo}
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+`
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertListToHtml).join('');
+        pokemonList.innerHTML += newHtml;
+    });
+}
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordNextPage = offset + limit;
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = qtdRecordNextPage - maxRecords;
+        loadPokemonItens(offset, newLimit); // Use newLimit if needed
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+});
